e5f0ae5b7f8f466b5d1501ebea6641049133f617
LorenitaTL <lore7096@gmail.com>
2020-08-11 16:19:49 -0500

    add docs

---
+++ docs/precache-manifest.889331b3b2a2d89ed38807082a01767d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e971623d25cd2c8ea115e75cacae519f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac9e659e6d31e6141e29",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5c52f9a594611f307e28",
+    "url": "/static/js/2.64c87fc6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.64c87fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac9e659e6d31e6141e29",
+    "url": "/static/js/main.44948717.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
